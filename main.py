72a72a702c3f312fcb10920226eaf3979470f150
dazemc <daazedjmcfarland@gmail.com>
2023-03-09 00:11:40 -0800

    Added some functionality with help menu

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import os
-import twilio.base.exceptions
 from twilio.rest import Client
 
 store_name = "Stacks"
 body = f"Your {store_name} order is ready for pickup!\n🍟🍔➡️😋"
 
+def get_number():
+    country_code = "+1"
+    cell_num = input("Enter Cellphone number: ")
+    if cell_num == "*":
+        print(
+            "Help:\n"
+            "'*' Prints this menu\n"
+            "'0000' Reboots device\n"
+            "'0001' Exits program"
+        )
+        get_number()
+    if cell_num == "0000":
+        return os.system("shutdown /r /t 1")
+    if cell_num == "0001":
+        quit()
+    else:
+        return country_code + cell_num
+
+
+
 while True:
-    rec_num = "+1" + input("Enter Cellphone number: ")
+    rec_num = get_number()
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
     auth_token = os.environ['TWILIO_AUTH_TOKEN']
     twilio_number = os.environ['TWILIO_NUMBER']
@@ -19,7 +38,7 @@ while True:
                                   )
         if message.status != "queued":
             print(f"Message not sent!\nReport: {message.status}")
-    except twilio.base.exceptions.TwilioRestException:
-        print(f"Number: {rec_num}\nThis is not a valid number")
-
-    print("Message sent!")
+    except Exception as error:
+        print(error)
+    else:
+        print("Message sent!")
